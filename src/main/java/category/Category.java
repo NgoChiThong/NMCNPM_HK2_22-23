0674c477d47f0d808cec1c4e8fea93aeed341ea3
=tranhuudan-02573 <=20130218@st.hcmuaf.edu.vn>
2023-05-09 01:34:11 +0700

    update

--- src/main/java/category/Category.java
+++ src/main/java/category/Category.java
@@ -1,8 +1,15 @@
 package category;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.sql.Timestamp;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
     private Integer id;
     private Timestamp created_at;
@@ -11,75 +18,4 @@ public class Category {
     private String type;
     private String icon;
 
-    @Override
-    public String toString() {
-        return "Category{" +
-                "id=" + id +
-                ", created_at=" + created_at +
-                ", updated_at=" + updated_at +
-                ", name='" + name + '\'' +
-                ", type='" + type + '\'' +
-                ", icon='" + icon + '\'' +
-                '}';
-    }
-
-    public Category() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public Timestamp getCreated_at() {
-        return created_at;
-    }
-
-    public Timestamp getUpdated_at() {
-        return updated_at;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String getIcon() {
-        return icon;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setCreated_at(Timestamp created_at) {
-        this.created_at = created_at;
-    }
-
-    public void setUpdated_at(Timestamp updated_at) {
-        this.updated_at = updated_at;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public void setIcon(String icon) {
-        this.icon = icon;
-    }
-
-    public Category(Integer id, Timestamp created_at, Timestamp updated_at, String name, String type, String icon) {
-        this.id = id;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
-        this.name = name;
-        this.type = type;
-        this.icon = icon;
-    }
 }
--- src/main/java/transaction/Transaction.java
+++ src/main/java/transaction/Transaction.java
@@ -3,10 +3,16 @@ package transaction;
 
 import category.Category;
 import DB.JDBiConnector;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import user.User;
 
 import java.sql.Timestamp;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Transaction {
     private Integer id;
     private Timestamp created_at;
@@ -18,13 +24,6 @@ public class Transaction {
 
     private Integer userId;
 
-    public Category getCategory() {
-        return _getCategory();
-    }
-
-    public User getUser() {
-        return _getUser();
-    }
 
     private Category _getCategory() {
         return JDBiConnector.get().withHandle(handle -> {
@@ -38,85 +37,5 @@ public class Transaction {
         });
     }
 
-
-    public Transaction(Integer id, Timestamp created_at, Timestamp updated_at, Integer money, Integer categoryId, String note, Timestamp started_at, Integer userId) {
-        this.id = id;
-        this.created_at = created_at;
-        this.updated_at = updated_at;
-        this.money = money;
-        this.categoryId = categoryId;
-        this.note = note;
-        this.started_at = started_at;
-        this.userId = userId;
-    }
-
-    public Transaction() {
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-
-    public void setCreated_at(Timestamp created_at) {
-        this.created_at = created_at;
-    }
-
-    public void setUpdated_at(Timestamp updated_at) {
-        this.updated_at = updated_at;
-    }
-
-    public void setMoney(Integer money) {
-        this.money = money;
-    }
-
-    public void setCategoryId(Integer categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public void setNote(String note) {
-        this.note = note;
-    }
-
-    public void setStarted_at(Timestamp started_at) {
-        this.started_at = started_at;
-    }
-
-
-    public Integer getId() {
-        return id;
-    }
-
-    public Timestamp getCreated_at() {
-        return created_at;
-    }
-
-    public Timestamp getUpdated_at() {
-        return updated_at;
-    }
-
-    public Integer getMoney() {
-        return money;
-    }
-
-    public Integer getCategoryId() {
-        return categoryId;
-    }
-
-    public String getNote() {
-        return note;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public Timestamp getStarted_at() {
-        return started_at;
-    }
 
 }
